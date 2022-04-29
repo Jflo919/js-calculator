007b9c310366a1c7b5ff1b1acb0099775f28dfe5
JulianF <floydjulian1991@gmail.com>
2022-04-29 15:26:33 -0700

    console.logs added to eventlistener to see if keys work, also had to use the lessons' data-action attribute in the html, so this exercise could work, I don't know what I'm talking about, here

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.keys');
+const keys = calculator.querySelector('.calculator__keys');
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         // more code here to make it do something
+        // have to add data-action attribute in the html to make this work.
+        
+        const key= e.target;
+        const action =  key.dataset.action;
+        
+        if (!action) {
+            console.log('number keys');
+        }
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide' 
+        ) {
+            console.log('Operator pressed!');
+        }
+        if (action === 'decimal') {
+            console.log('decimal key was pressed');
+        }
+        if (action === 'clear') {
+            console.log('clear key pressed!');
+        }
+        if (action === 'calculate') {
+        console.log('equals key pressed!');  
+        }
     }
 })
