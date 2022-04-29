f1512713eb39dff1bb0b5c77664988ae711d4951
JulianF <floydjulian1991@gmail.com>
2022-04-28 20:54:35 -0700

    typed the css from source code for funsies

---
+++ script.js
@@ -0,0 +1,8 @@
+const calculator = document.querySelector('.calculator');
+const keys = calculator.querySelector('.keys');
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        // more code here to make it do something
+    }
+})
